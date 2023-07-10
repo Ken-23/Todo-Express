d671a636e6b73ed7d4f6fc0ab4eeeb3d47ee6ea4
Ken <kaloniaken@gmail.com>
2023-07-11 00:41:52 +0530

    mongo db key removed

--- config/db.js
+++ config/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = () => {
   try {
-    mongoose.connect(
-      "mongodb+srv://kaloniaken:GQnRAzgwhI7Ws5v1@cluster0.bnyrpeo.mongodb.net/?retryWrites=true&w=majority"
-    );
+    mongoose.connect();
     console.log("MongoDB connected!");
   } catch (err) {
     console.log(err);
